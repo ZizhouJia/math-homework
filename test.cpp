edf260fd8e4b916d9df7a2369408ea60f860d3c5
XiaoyuHuang96 <44343235+XiaoyuHuang96@users.noreply.github.com>
2018-10-22 00:55:07 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -47,7 +47,7 @@ int main(){
       permutation_number number=generate_permutation_dict(i,N[n]);
       long index=from_permutation_dict(number);
       if(index!=i){
-        std::cout<<"permutation 2 error in test N="<<std::to_string(N[n])<<std::endl;
+        std::cout<<"permutation dict error in test N="<<std::to_string(N[n])<<std::endl;
         std::cout<<"error in ("<<std::to_string(index)<<","<<std::to_string(i)<<")"<<std::endl;
         std::cout<<number.to_string()<<std::endl;
         succeed=0;
@@ -59,7 +59,7 @@ int main(){
       permutation_number number=generate_permutation_increase(i,N[n]);
       long index=from_permutation_increase(number);
       if(index!=i){
-        std::cout<<"permutation 2 error in test N="<<std::to_string(N[n])<<std::endl;
+        std::cout<<"permutation increase error in test N="<<std::to_string(N[n])<<std::endl;
         std::cout<<"error in ("<<std::to_string(index)<<","<<std::to_string(i)<<")"<<std::endl;
         std::cout<<number.to_string()<<std::endl;
         succeed=0;
