e5baffae068ddb5bd85ba7a7a64ad103980ec76e
XiaoyuHuang96 <44343235+XiaoyuHuang96@users.noreply.github.com>
2018-10-22 00:54:25 +0800

    add increase media number

--- test.cpp
+++ test.cpp
@@ -42,6 +42,30 @@ int main(){
         break;
       }
     }
+    
+    for(long i=0;i<count;++i){
+      permutation_number number=generate_permutation_dict(i,N[n]);
+      long index=from_permutation_dict(number);
+      if(index!=i){
+        std::cout<<"permutation 2 error in test N="<<std::to_string(N[n])<<std::endl;
+        std::cout<<"error in ("<<std::to_string(index)<<","<<std::to_string(i)<<")"<<std::endl;
+        std::cout<<number.to_string()<<std::endl;
+        succeed=0;
+        break;
+      }
+    }
+    
+    for(long i=0;i<count;++i){
+      permutation_number number=generate_permutation_increase(i,N[n]);
+      long index=from_permutation_increase(number);
+      if(index!=i){
+        std::cout<<"permutation 2 error in test N="<<std::to_string(N[n])<<std::endl;
+        std::cout<<"error in ("<<std::to_string(index)<<","<<std::to_string(i)<<")"<<std::endl;
+        std::cout<<number.to_string()<<std::endl;
+        succeed=0;
+        break;
+      }
+    }
   }
   if(succeed){
     std::cout<<"all test succeed"<<std::endl;
