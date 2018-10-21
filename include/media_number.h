61a70ad21b3870e4c805b71ceeca8d5d9c737796
XiaoyuHuang96 <44343235+XiaoyuHuang96@users.noreply.github.com>
2018-10-22 00:37:34 +0800

    Update media_number.h
    add increase media number

--- include/media_number.h
+++ include/media_number.h
@@ -27,24 +27,24 @@ class decrease_media_number{
 
 class increase_media_number
 {
-	public:
-		//N代表为N个数的全排列
-		//number为升序中介数的10进制表示
-		increase_media_number(long number, int N);
-		increase_media_number(vector<int> numbers, int N);
-		//返回该数的十进制表示
-		long to_number();
-		//返回该数的字符串
-		string to_string();
-		//获取该数的vector
-		vector<int> get_vector();
-		//获取全排列的位
-		int get_N();
-		~increase_media_number();
-	private:
-		int N;
-		vector<int> numbers;
-		long factorial;
+  public:
+    //N代表为N个数的全排列
+    //number为升序中介数的10进制表示
+    increase_media_number(long number, int N);
+    increase_media_number(vector<int> numbers, int N);
+    //返回该数的十进制表示
+    long to_number();
+    //返回该数的字符串
+    string to_string();
+    //获取该数的vector
+    vector<int> get_vector();
+    //获取全排列的位
+    int get_N();
+    ~increase_media_number();
+  private:
+    int N;
+    vector<int> numbers;
+    long factorial;
 };
 
 long get_factorial(int N);
